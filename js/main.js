693f811b8f77946e8ba610b0b0367d806081b0e8
DevLucianoPrandi <devlucianoprandi@gmail.com>
2022-11-01 11:47:23 -0300

    sube3

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
-let nombre = prompt("Cuál es tu nombre?");
-if (nombre != "") {
-    return nombre
+let nombre = prompt("¿Cuál es tu nombre?");
+ if (nombre != "") {
+  console.log(nombre)
+
 } else {
   let nombre = prompt("Por favor, ingresá un nombre");
-  return nombre
-}
+ console.log (nombre)
+
+};
+let titulo=document.getElementById("titulo");
+titulo.innerHTML= `<h1>Bienvenid@ a <b>Interlingua</b>, ${nombre}</h1>`;
